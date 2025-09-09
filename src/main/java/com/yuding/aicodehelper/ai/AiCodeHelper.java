497379966c08b7f77da6d05441ed41119c352eea
Yuding <steven_yuding@163.com>
2025-09-09 21:02:57 +0800

    多模态对话与系统提示词

--- src/main/java/com/yuding/aicodehelper/ai/AiCodeHelper.java
+++ src/main/java/com/yuding/aicodehelper/ai/AiCodeHelper.java
@@ -1,6 +1,7 @@
 package com.yuding.aicodehelper.ai;
 
 import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.response.ChatResponse;
@@ -24,8 +25,21 @@ public class AiCodeHelper {
     @Resource
     private ChatModel qwenChatModel;
 
+    // PROMPT
+    private static final String SYSTEM_MESSAGE = "你是一个AI代码助手，请根据用户输入的代码问题，给出对应的代码";
+
+
+    //简单对话
     public String chat(String message){
+        SystemMessage systemMessage = SystemMessage.from(SYSTEM_MESSAGE);
         UserMessage userMessage = UserMessage.from(message);
+        ChatResponse chatResponse = qwenChatModel.chat(systemMessage, userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("aiMessage: {}", aiMessage);
+        return aiMessage.text();
+    }
+    //多模态用户信息对话
+    public String chatWithUserMessage(UserMessage userMessage){
         ChatResponse chatResponse = qwenChatModel.chat(userMessage);
         AiMessage aiMessage = chatResponse.aiMessage();
         log.info("aiMessage: {}", aiMessage);
--- src/test/java/com/yuding/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/yuding/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,6 +1,9 @@
 package com.yuding.aicodehelper;
 
 import com.yuding.aicodehelper.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,5 +16,15 @@ class AiCodeHelperApplicationTests {
     @Test
     void chat() {
         aiCodeHelper.chat("写一个java程序，打印1到10的偶数");
+    }
+
+    @Test
+    void chatWithUserMessage() {
+        // 采用的模型不支持多模态输入，这里仅供测试使用
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("解释这个图片"),
+                ImageContent.from("")
+        );
+        aiCodeHelper.chatWithUserMessage(userMessage);
     }
 }
